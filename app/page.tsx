402457191b89767cee2e612e4a9776d15747caeb
David Newman <davidn988878@gmail.com>
2023-09-03 13:30:04 -0700

    create LoadMore/pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { ProjectInterface } from "@/common.types";
 import Categories from "@/components/Categories";
+import LoadMore from "@/components/LoadMore";
 import ProjectCard from "@/components/ProjectCard";
 import { fetchAllProjects } from "@/lib/actions";
 
@@ -15,21 +16,35 @@ type ProjectSearch = {
     }
 }
 
-const Home = async () => {
-    const data = await fetchAllProjects() as ProjectSearch;
+type SearchParams = {
+    category?: string;
+    endcursor?: string;
+}
+
+type Props = {
+    searchParams: SearchParams
+}
+
+export const dynamic = 'force-dynamic';
+export const dynamicParams = true;
+export const revalidate = 0;
+
+const Home = async ({ searchParams: { category, endcursor }}: Props) => {
+    const data = await fetchAllProjects(category, endcursor) as ProjectSearch;
 
     const projectsToDisplay = data?.projectSearch?.edges || [];
 
     if(projectsToDisplay.length === 0) {
         return(
             <section className="flexStart flex-col paddings">
-                Categories
+                <Categories />
 
                 <p className="no-result-test text-center">No projects found, go create some first.</p>
-
+  
             </section>
         )
     }
+    const pagination = data?.projectSearch?.pageInfo;
 
     return (
         <section className="flex-start flex-col paddings mb-16">
@@ -48,7 +63,12 @@ const Home = async () => {
                     />
                 ))}
             </section>
-            <h1>LoadMore</h1>
+            <LoadMore 
+                startCursor={pagination.startCursor}
+                endCursor={pagination.endCursor}
+                hasPreviousPage={pagination.hasPreviousPage}
+                hasNextPage={pagination.hasNextPage}
+            />
         </section>
     )
 }
---
+++ app/profile/[id]/page.tsx
@@ -0,0 +1,15 @@
+import { getUserProjects } from "@/lib/actions"
+
+type Props = {
+    params: {
+        id: string
+    }
+}
+
+const UserProfile = () => {
+  return (
+    <div>UserProfile</div>
+  )
+}
+
+export default UserProfile
