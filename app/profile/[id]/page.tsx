ff23796058b4311f50db743f2d77c809d3d6164e
David Newman <davidn988878@gmail.com>
2023-09-03 13:36:54 -0700

    create ProfilePage component/end of tutorial

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,3 +1,5 @@
+import { UserProfile } from "@/common.types"
+import ProfilePage from "@/components/ProfilePage"
 import { getUserProjects } from "@/lib/actions"
 
 type Props = {
@@ -6,10 +8,14 @@ type Props = {
     }
 }
 
-const UserProfile = () => {
-  return (
-    <div>UserProfile</div>
-  )
+const UserProfile = async ({ params }: Props) => {
+    const result = await getUserProjects(params.id, 100) as { user: UserProfile }
+
+    if(!result?.user) {
+        return <p className="no-result-text">Failed to fetch user info</p>
+    }
+
+  return <ProfilePage user={result?.user} />
 }
 
 export default UserProfile
